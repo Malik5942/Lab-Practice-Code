fcf41da20243d099f8902aae098e4c8a89d4fcfe
Mudassir Malik <46223724+Malik5942@users.noreply.github.com>
2018-12-29 10:25:44 +0500

    File Handling full 28-12-2018

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ server.use(bodyParser.urlencoded())
 server.use(bodyParser.json())
 server.use(express.static('./frontend'))
 
-var users = ['Mudassir', "Rizwan", 'shoaib']
+// var users = ['Mudassir', "Rizwan", 'shoaib']
+var users = [{username: 'Malik', passwoed: 123}]
 
 server.get('/getAllUsers', (req, res)=>{
     res.send(users)
@@ -15,19 +16,29 @@ server.get('/getAllUsers', (req, res)=>{
 
 server.post('/addUser', (req, res)=>{
 
-    // users.push('Raheel') without name field
+    // users.push('Raheel')                                 without name field
+
+    // users.push(req.body.username)                         with only one field
     
-    users.push(req.body.username)
-    
+    let newUser = {username: req.body.username, password: req.body.password} 
+    users.push(newUser)
     res.end('User is added')
 })
 
 // File Handling is going to start from here
 
 server.post('/createFile', (req, res)=>{
-    fs.appendFile('myFiles/myNewfile.txt', "Hello Everyone" + '\r\n', function (err){
+    fs.writeFile('myfiles/myNewfile.txt', "Hello Everyone", function (err){
         if(err) throw err;
-           res.send('File successfully created')
+        res.send('File successfully created')
+    });
+})
+
+
+server.get('/updateFile', (req, res)=>{
+    fs.appendFile('myfiles/myNewfile.txt', "Hello Everyone  once again" + '\r\n', function (err){
+        if(err) throw err;
+           res.send('File successfully updated')
     });
  })
                 
